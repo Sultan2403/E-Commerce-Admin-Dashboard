e902c772410f128db227e4c55e25eb27e70bd21e
oreoluwaisagirl <oreoluwaisagirl@gmail.com>
2025-12-01 14:10:58 +0100

    corrected dependencies err...

--- src/UI/Search Filter/searchFilter.jsx
+++ src/UI/Search Filter/searchFilter.jsx
@@ -1,6 +1,6 @@
 
 import { useState } from "react";
-import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+// import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 
 export default function SearchFilter({ onSearch, onCategoryChange }) {
   const [query, setQuery] = useState("");
@@ -11,7 +11,7 @@ export default function SearchFilter({ onSearch, onCategoryChange }) {
 
       {/* Search Box */}
       <div className="relative flex-1">
-        <MagnifyingGlassIcon className="w-5 h-5 text-gray-500 absolute left-3 top-1/2 -translate-y-1/2" />
+        {/* <MagnifyingGlassIcon className="w-5 h-5 text-gray-500 absolute left-3 top-1/2 -translate-y-1/2" /> */}
         <input
           type="text"
           placeholder="Search..."
