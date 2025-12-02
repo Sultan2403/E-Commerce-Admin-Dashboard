628344a14b86e5c95d9364783184a70f1e263f12
oreoluwaisagirl <oreoluwaisagirl@gmail.com>
2025-12-02 23:06:04 +0100

    i just commited the button

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Header from "./components/Header";
 import DisplayProducts from "./UI/Products Display/displayproducts";
 import Nav from "./UI/Navigation/nav";
 import SearchFilter from "./UI/Search Filter/searchFilter";
+import Button from "./UI/addButton/button";
 
 function App() {
   return (
@@ -24,6 +25,7 @@ function App() {
         </div>
         <SearchFilter/>
         <DisplayProducts />
+        <Button/>
       </div>
     </div>
   );
