6ac80096f31c3646e681e7cfc1cbe6f01dd74a1b
Sultan2403 <sultanremilekun@gmail.com>
2025-12-10 20:57:27 +0100

    feat:  added dashboard component and implemented dashboard routing

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import DisplayProducts from "./Components/UI/Pages/Products Page/Products Display/displayproducts";
 import Orders from "./Components/UI/Pages/Orders Page/orders";
 import AdminLayout from "./Components/UI/Static/Main/AdminLayout";
+import Dashboard from "./Components/UI/Pages/Dashboard/Dashboard";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<AdminLayout />}>
-          <Route path="products" index element={<DisplayProducts />}></Route>
+          <Route index element={<Dashboard />}></Route>
+          <Route path="products" element={<DisplayProducts />}></Route>
           <Route path="orders" element={<Orders />}></Route>
         </Route>
       </Routes>
