982cc1407500295eb0180f4e8e8459c04000c7e5
Ahmad <tajudeenahmadolanrewaju@gmail.com>
2025-12-15 09:43:41 +0100

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,33 +1,35 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import { AppProvider } from "./context/AppContext";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <AppProvider>
-      <App />
-    </AppProvider>
-  </React.StrictMode>
+createRoot(document.getElementById("root")).render(
+  // <StrictMode>
+  <App />
+  // </StrictMode>,
 );
 
 
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import App from "./App";
+// //import { AppProvider } from "./context/AppContext";
 
-
-
-
-
-
-
-
-
-// import { StrictMode } from "react";
-// import { createRoot } from "react-dom/client";
-// import "./index.css";
-// import App from "./App.jsx";
-
-// createRoot(document.getElementById("root")).render(
-//   // <StrictMode>
-//   <App />
-//   // </StrictMode>,
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//   <React.StrictMode>
+//     <AppProvider>
+//       <App />
+//     </AppProvider> 
+//   </React.StrictMode>
 // );
+
+
+
+
+
+
+
+
+
+
+
