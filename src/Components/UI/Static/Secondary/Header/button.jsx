140fee1db7dc89bbf53f3be4f0b0ca28c3d09476
Sultan2403 <sultanremilekun@gmail.com>
2026-01-09 11:34:53 +0100

    Deployment ready

--- src/Components/UI/Static/Secondary/Header/button.jsx
+++
@@ -1,7 +0,0 @@
-export default function Button() {
-  return (
-    <button className="font-semibold flex gap-1 items-center justify-center text-md bg-blue-600 hover:bg-blue-700 text-white px-[12px] py-[10px] rounded-2xl transition-all">
-      <span>+</span> <span>Add New Product</span>
-    </button>
-  );
-}
