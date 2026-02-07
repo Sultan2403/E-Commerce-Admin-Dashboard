8cf83c41cfe1740d1a5dc43a62b47b165e21b2de
Sultan2403 <sultanremilekun@gmail.com>
2026-02-07 23:39:48 +0100

    Basic backend scaffolding and gh workflows setup

---
+++ Backend/app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+const { errors } = require("celebrate");
+
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://sultan2403.github.io"],
+  }),
+);
+app.use(express.json());
+
+
+
+app.get("/", (req, res) => {
+  res
+    .status(200)
+    .json({ message: " Looking for something? Well it's not here XD" });
+});
+
+app.get("/health", (req, res) => {
+  res.status(200).json({ message: "Server says heyyy :)" });
+});
+
+
+
+app.use(errors()); 
+
+module.exports = app;
